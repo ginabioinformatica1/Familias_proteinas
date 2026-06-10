b3ccf8ab30c0540452f9de9a643aaf60b181b704
Ginamaria <ginabioinformatica@gmail.com>
2026-06-10 11:40:25 -0500

    Cambio en codigo de transformar tsv

--- pipeline/Transformar_tsv.R
+++ pipeline/Transformar_tsv.R
@@ -1,53 +1,54 @@
-transformar_a_tsv <- function(archivo_entrada, archivo_salida) {
+# Archivo de entrada y salida fijos
+archivo_entrada <- "resultados/predicciones_totales.txt"
+archivo_salida <- "resultados/predicciones_totales.tsv"
 
-  # Leer todas las líneas
-  lineas <- readLines(archivo_entrada)
+# Leer todas las líneas
+lineas <- readLines(archivo_entrada)
 
-  # Eliminar comentarios (#)
-  lineas_datos <- lineas[!grepl("^#", lineas)]
+# Eliminar comentarios (líneas que comienzan con #)
+lineas_datos <- lineas[!grepl("^#", lineas)]
 
-  # Eliminar líneas vacías
-  lineas_datos <- lineas_datos[nchar(trimws(lineas_datos)) > 0]
+# Eliminar líneas vacías
+lineas_datos <- lineas_datos[nchar(trimws(lineas_datos)) > 0]
 
-  # Separar columnas por uno o más espacios
-  tabla <- read.table(
-    text = lineas_datos,
-    header = FALSE,
-    fill = TRUE,
-    stringsAsFactors = FALSE
-  )
+# Convertir a tabla
+tabla <- read.table(
+  text = lineas_datos,
+  header = FALSE,
+  fill = TRUE,
+  stringsAsFactors = FALSE
+)
 
-  # Asignar nombres de columnas de tblout
-  colnames(tabla) <- c(
-    "target_name",
-    "target_accession",
-    "query_name",
-    "query_accession",
-    "E_value",
-    "score",
-    "bias",
-    "E_value_domain",
-    "score_domain",
-    "bias_domain",
-    "exp",
-    "reg",
-    "clu",
-    "ov",
-    "env",
-    "dom",
-    "rep",
-    "inc",
-    "descripcion"
-  )
+# Nombres de columnas
+colnames(tabla) <- c(
+  "target_name",
+  "target_accession",
+  "query_name",
+  "query_accession",
+  "E_value",
+  "score",
+  "bias",
+  "E_value_domain",
+  "score_domain",
+  "bias_domain",
+  "exp",
+  "reg",
+  "clu",
+  "ov",
+  "env",
+  "dom",
+  "rep",
+  "inc",
+  "descripcion"
+)
 
-  # Guardar como TSV
-  write.table(
-    tabla,
-    archivo_salida,
-    sep = "\t",
-    row.names = FALSE,
-    quote = FALSE
-  )
+# Guardar como TSV
+write.table(
+  tabla,
+  archivo_salida,
+  sep = "\t",
+  row.names = FALSE,
+  quote = FALSE
+)
 
-  cat("Archivo TSV generado:", archivo_salida, "\n")
-}
+cat("Archivo TSV generado correctamente en:", archivo_salida, "\n")
